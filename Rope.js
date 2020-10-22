e69b73a63faadcf3025591abf2b926dc2e6ba653
Abyan1234 <70338733+Abyan1234@users.noreply.github.com>
2020-10-22 17:17:25 -0400

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,24 @@
+class Rope {
+
+
+    constructor(bodyA,bodyB){
+    
+    var options ={
+    
+        bodyA:bodyA,
+        bodyB:bodyB,
+        
+        length:200
+    }
+    
+    this.rope=Constraint.create(options);
+    World.add(world,this.rope);
+    }
+    display(){
+    
+    var pointA = this.rope.bodyA.position;
+    var pointB = this.rope.bodyB.position;
+    strokeWeight(4); 
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+     }
+    }
